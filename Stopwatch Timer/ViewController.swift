a230d978f7c66789fd8b8021c7891eb8a3fe486c
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:35:47 +0530

    Stopwatch

--- Stopwatch Timer/ViewController.swift
+++ Stopwatch Timer/ViewController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    var timer = NSTimer()
+    var sec = 0
+    var min = 0
+    
+    @IBAction func start(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    
+    @IBAction func reset(sender: AnyObject) {
+        
+        timer.invalidate()
+        sec = 0
+        min = 0
+        outputLabel.text = "\(min) m \(sec) s"
+    }
+    func increaseTimer() {
+        
+        getOutput()
+        outputLabel.text = "\(min) m \(sec) s"
+        sec++
+    }
+    
+    func getOutput() {
+        
+        if sec == 60 {
+            sec = 0
+            min++
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
